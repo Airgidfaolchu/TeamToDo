81334ea5953dce74ea905aa3a1fe083c52fe27b9
Attila <Airgidfaolchu@users.noreply.github.com>
2014-11-20 11:51:58 +0000

    updated password

--- public/javascripts/database.js
+++ public/javascripts/database.js
@@ -1,6 +1,6 @@
 // javascripts/database.js
 module.exports = {
 
-	'url' : 'mongodb://Attila:Hokicsapat@ds031628.mongolab.com:31628/shopping_db_v2'
+	'url' : 'mongodb://Attila:Hokicsapat1@ds031628.mongolab.com:31628/shopping_db_v2'
 
 };
